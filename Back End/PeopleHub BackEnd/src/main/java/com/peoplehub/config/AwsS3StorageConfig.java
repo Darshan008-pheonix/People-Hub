d730d5a703e7b4a496669ef1c4dd385ce0c8353e
Darshan008-pheonix <www.555darshanrdn@gmail.com>
2024-04-27 18:36:23 +0530

    done

--- Back End/PeopleHub BackEnd/src/main/java/com/peoplehub/config/AwsS3StorageConfig.java
+++ Back End/PeopleHub BackEnd/src/main/java/com/peoplehub/config/AwsS3StorageConfig.java
@@ -12,21 +12,4 @@ import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Builder;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 
-@Configuration
-public class AwsS3StorageConfig {
 
-	//@Value("${cloud.aws.credentials.access-key}")
-	private String accessKey="AKIA6N6B5OYKWQYPI37N";
-	
-	//@Value("${cloud.aws.credentials.screte-key}")
-	private String accessSecret="deayGrzVOZxph3+ByGUjEgJe26mimItth89iLvEy";
-	
-	//@Value("${cloud.aws.region.static}")
-	private String region="us-east-2";
-	
-	@Bean
-	public AmazonS3 generateS3Client() {
-		AWSCredentials credentials=new BasicAWSCredentials(accessKey, accessSecret);
-		return AmazonS3ClientBuilder.standard().withCredentials(new AWSStaticCredentialsProvider(credentials)).withRegion(region).build();
-	}
-}
